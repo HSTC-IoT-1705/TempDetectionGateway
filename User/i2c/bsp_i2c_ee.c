8d9530396e32fba4dacdafa3bc0600e50aac4b34
Gump <870705273@qq.com>
2020-05-02 14:36:47 +0800

    feat: complie successfully

--- User/i2c/bsp_i2c_ee.c
+++ User/i2c/bsp_i2c_ee.c
@@ -3,13 +3,13 @@
   * @file    bsp_i2c_ee.c
   * @version V1.0
   * @date    2013-xx-xx
-  * @brief   i2c EEPROM(AT24C02)应用函数bsp
+  * @brief   i2c EEPROM(AT24C02)Ӧ�ú���bsp
   ******************************************************************************
   * @attention
   *
-  * 实验平台:野火 F103-指南者 STM32 开发板 
-  * 论坛    :http://www.firebbs.cn
-  * 淘宝    :https://fire-stm32.taobao.com
+  * ʵ��ƽ̨:Ұ�� F103-ָ���� STM32 ������ 
+  * ��̳    :http://www.firebbs.cn
+  * �Ա�    :https://fire-stm32.taobao.com
   *
   ******************************************************************************
   */ 
@@ -20,10 +20,10 @@
 
 /*
 *********************************************************************************************************
-*	函 数 名: ee_CheckOk
-*	功能说明: 判断串行EERPOM是否正常
-*	形    参：无
-*	返 回 值: 1 表示正常， 0 表示不正常
+*	�� �� ��: ee_CheckOk
+*	����˵��: �жϴ���EERPOM�Ƿ�����
+*	��    �Σ���
+*	�� �� ֵ: 1 ��ʾ������ 0 ��ʾ������
 *********************************************************************************************************
 */
 uint8_t ee_CheckOk(void)
@@ -34,7 +34,7 @@ uint8_t ee_CheckOk(void)
 	}
 	else
 	{
-		/* 失败后，切记发送I2C总线停止信号 */
+		/* ʧ�ܺ��мǷ���I2C����ֹͣ�ź� */
 		i2c_Stop();		
 		return 0;
 	}
@@ -42,86 +42,86 @@ uint8_t ee_CheckOk(void)
 
 /*
 *********************************************************************************************************
-*	函 数 名: ee_ReadBytes
-*	功能说明: 从串行EEPROM指定地址处开始读取若干数据
-*	形    参：_usAddress : 起始地址
-*			 _usSize : 数据长度，单位为字节
-*			 _pReadBuf : 存放读到的数据的缓冲区指针
-*	返 回 值: 0 表示失败，1表示成功
+*	�� �� ��: ee_ReadBytes
+*	����˵��: �Ӵ���EEPROMָ����ַ����ʼ��ȡ��������
+*	��    �Σ�_usAddress : ��ʼ��ַ
+*			 _usSize : ���ݳ��ȣ���λΪ�ֽ�
+*			 _pReadBuf : ��Ŷ��������ݵĻ�����ָ��
+*	�� �� ֵ: 0 ��ʾʧ�ܣ�1��ʾ�ɹ�
 *********************************************************************************************************
 */
 uint8_t ee_ReadBytes(uint8_t *_pReadBuf, uint16_t _usAddress, uint16_t _usSize)
 {
 	uint16_t i;
 	
-	/* 采用串行EEPROM随即读取指令序列，连续读取若干字节 */
+	/* ���ô���EEPROM�漴��ȡָ�����У�������ȡ�����ֽ� */
 	
-	/* 第1步：发起I2C总线启动信号 */
+	/* ��1��������I2C���������ź� */
 	i2c_Start();
 	
-	/* 第2步：发起控制字节，高7bit是地址，bit0是读写控制位，0表示写，1表示读 */
-	i2c_SendByte(EEPROM_DEV_ADDR | EEPROM_I2C_WR);	/* 此处是写指令 */
+	/* ��2������������ֽڣ���7bit�ǵ�ַ��bit0�Ƕ�д����λ��0��ʾд��1��ʾ�� */
+	i2c_SendByte(EEPROM_DEV_ADDR | EEPROM_I2C_WR);	/* �˴���дָ�� */
 	 
-	/* 第3步：等待ACK */
+	/* ��3�����ȴ�ACK */
 	if (i2c_WaitAck() != 0)
 	{
-		goto cmd_fail;	/* EEPROM器件无应答 */
+		goto cmd_fail;	/* EEPROM������Ӧ�� */
 	}
 
-	/* 第4步：发送字节地址，24C02只有256字节，因此1个字节就够了，如果是24C04以上，那么此处需要连发多个地址 */
+	/* ��4���������ֽڵ�ַ��24C02ֻ��256�ֽڣ����1���ֽھ͹��ˣ������24C04���ϣ���ô�˴���Ҫ���������ַ */
 	i2c_SendByte((uint8_t)_usAddress);
 	
-	/* 第5步：等待ACK */
+	/* ��5�����ȴ�ACK */
 	if (i2c_WaitAck() != 0)
 	{
-		goto cmd_fail;	/* EEPROM器件无应答 */
+		goto cmd_fail;	/* EEPROM������Ӧ�� */
 	}
 	
-	/* 第6步：重新启动I2C总线。前面的代码的目的向EEPROM传送地址，下面开始读取数据 */
+	/* ��6������������I2C���ߡ�ǰ��Ĵ����Ŀ����EEPROM���͵�ַ�����濪ʼ��ȡ���� */
 	i2c_Start();
 	
-	/* 第7步：发起控制字节，高7bit是地址，bit0是读写控制位，0表示写，1表示读 */
-	i2c_SendByte(EEPROM_DEV_ADDR | EEPROM_I2C_RD);	/* 此处是读指令 */
+	/* ��7������������ֽڣ���7bit�ǵ�ַ��bit0�Ƕ�д����λ��0��ʾд��1��ʾ�� */
+	i2c_SendByte(EEPROM_DEV_ADDR | EEPROM_I2C_RD);	/* �˴��Ƕ�ָ�� */
 	
-	/* 第8步：发送ACK */
+	/* ��8��������ACK */
 	if (i2c_WaitAck() != 0)
 	{
-		goto cmd_fail;	/* EEPROM器件无应答 */
+		goto cmd_fail;	/* EEPROM������Ӧ�� */
 	}	
 	
-	/* 第9步：循环读取数据 */
+	/* ��9����ѭ����ȡ���� */
 	for (i = 0; i < _usSize; i++)
 	{
-		_pReadBuf[i] = i2c_ReadByte();	/* 读1个字节 */
+		_pReadBuf[i] = i2c_ReadByte();	/* ��1���ֽ� */
 		
-		/* 每读完1个字节后，需要发送Ack， 最后一个字节不需要Ack，发Nack */
+		/* ÿ����1���ֽں���Ҫ����Ack�� ���һ���ֽڲ���ҪAck����Nack */
 		if (i != _usSize - 1)
 		{
-			i2c_Ack();	/* 中间字节读完后，CPU产生ACK信号(驱动SDA = 0) */
+			i2c_Ack();	/* �м��ֽڶ����CPU����ACK�ź�(����SDA = 0) */
 		}
 		else
 		{
-			i2c_NAck();	/* 最后1个字节读完后，CPU产生NACK信号(驱动SDA = 1) */
+			i2c_NAck();	/* ���1���ֽڶ����CPU����NACK�ź�(����SDA = 1) */
 		}
 	}
-	/* 发送I2C总线停止信号 */
+	/* ����I2C����ֹͣ�ź� */
 	i2c_Stop();
-	return 1;	/* 执行成功 */
+	return 1;	/* ִ�гɹ� */
 
-cmd_fail: /* 命令执行失败后，切记发送停止信号，避免影响I2C总线上其他设备 */
-	/* 发送I2C总线停止信号 */
+cmd_fail: /* ����ִ��ʧ�ܺ��мǷ���ֹͣ�źţ�����Ӱ��I2C�����������豸 */
+	/* ����I2C����ֹͣ�ź� */
 	i2c_Stop();
 	return 0;
 }
 
 /*
 *********************************************************************************************************
-*	函 数 名: ee_WriteBytes
-*	功能说明: 向串行EEPROM指定地址写入若干数据，采用页写操作提高写入效率
-*	形    参：_usAddress : 起始地址
-*			 _usSize : 数据长度，单位为字节
-*			 _pWriteBuf : 存放读到的数据的缓冲区指针
-*	返 回 值: 0 表示失败，1表示成功
+*	�� �� ��: ee_WriteBytes
+*	����˵��: ����EEPROMָ����ַд���������ݣ�����ҳд�������д��Ч��
+*	��    �Σ�_usAddress : ��ʼ��ַ
+*			 _usSize : ���ݳ��ȣ���λΪ�ֽ�
+*			 _pWriteBuf : ��Ŷ��������ݵĻ�����ָ��
+*	�� �� ֵ: 0 ��ʾʧ�ܣ�1��ʾ�ɹ�
 *********************************************************************************************************
 */
 uint8_t ee_WriteBytes(uint8_t *_pWriteBuf, uint16_t _usAddress, uint16_t _usSize)
@@ -130,33 +130,33 @@ uint8_t ee_WriteBytes(uint8_t *_pWriteBuf, uint16_t _usAddress, uint16_t _usSize
 	uint16_t usAddr;
 	
 	/* 
-		写串行EEPROM不像读操作可以连续读取很多字节，每次写操作只能在同一个page。
-		对于24xx02，page size = 8
-		简单的处理方法为：按字节写操作模式，没写1个字节，都发送地址
-		为了提高连续写的效率: 本函数采用page wirte操作。
+		д����EEPROM�������������������ȡ�ܶ��ֽڣ�ÿ��д����ֻ����ͬһ��page��
+		����24xx02��page size = 8
+		�򵥵Ĵ�������Ϊ�����ֽ�д����ģʽ��ûд1���ֽڣ������͵�ַ
+		Ϊ���������д��Ч��: ����������page wirte������
 	*/
 
 	usAddr = _usAddress;	
 	for (i = 0; i < _usSize; i++)
 	{
-		/* 当发送第1个字节或是页面首地址时，需要重新发起启动信号和地址 */
+		/* �����͵�1���ֽڻ���ҳ���׵�ַʱ����Ҫ���·��������źź͵�ַ */
 		if ((i == 0) || (usAddr & (EEPROM_PAGE_SIZE - 1)) == 0)
 		{
-			/*　第０步：发停止信号，启动内部写操作　*/
+			/*���ڣ�������ֹͣ�źţ������ڲ�д������*/
 			i2c_Stop();
 			
-			/* 通过检查器件应答的方式，判断内部写操作是否完成, 一般小于 10ms 			
-				CLK频率为200KHz时，查询次数为30次左右
+			/* ͨ���������Ӧ��ķ�ʽ���ж��ڲ�д�����Ƿ����, һ��С�� 10ms 			
+				CLKƵ��Ϊ200KHzʱ����ѯ����Ϊ30������
 			*/
 			for (m = 0; m < 1000; m++)
 			{				
-				/* 第1步：发起I2C总线启动信号 */
+				/* ��1��������I2C���������ź� */
 				i2c_Start();
 				
-				/* 第2步：发起控制字节，高7bit是地址，bit0是读写控制位，0表示写，1表示读 */
-				i2c_SendByte(EEPROM_DEV_ADDR | EEPROM_I2C_WR);	/* 此处是写指令 */
+				/* ��2������������ֽڣ���7bit�ǵ�ַ��bit0�Ƕ�д����λ��0��ʾд��1��ʾ�� */
+				i2c_SendByte(EEPROM_DEV_ADDR | EEPROM_I2C_WR);	/* �˴���дָ�� */
 				
-				/* 第3步：发送一个时钟，判断器件是否正确应答 */
+				/* ��3��������һ��ʱ�ӣ��ж������Ƿ���ȷӦ�� */
 				if (i2c_WaitAck() == 0)
 				{
 					break;
@@ -164,37 +164,37 @@ uint8_t ee_WriteBytes(uint8_t *_pWriteBuf, uint16_t _usAddress, uint16_t _usSize
 			}
 			if (m  == 1000)
 			{
-				goto cmd_fail;	/* EEPROM器件写超时 */
+				goto cmd_fail;	/* EEPROM����д��ʱ */
 			}
 		
-			/* 第4步：发送字节地址，24C02只有256字节，因此1个字节就够了，如果是24C04以上，那么此处需要连发多个地址 */
+			/* ��4���������ֽڵ�ַ��24C02ֻ��256�ֽڣ����1���ֽھ͹��ˣ������24C04���ϣ���ô�˴���Ҫ���������ַ */
 			i2c_SendByte((uint8_t)usAddr);
 			
-			/* 第5步：等待ACK */
+			/* ��5�����ȴ�ACK */
 			if (i2c_WaitAck() != 0)
 			{
-				goto cmd_fail;	/* EEPROM器件无应答 */
+				goto cmd_fail;	/* EEPROM������Ӧ�� */
 			}
 		}
 	
-		/* 第6步：开始写入数据 */
+		/* ��6������ʼд������ */
 		i2c_SendByte(_pWriteBuf[i]);
 	
-		/* 第7步：发送ACK */
+		/* ��7��������ACK */
 		if (i2c_WaitAck() != 0)
 		{
-			goto cmd_fail;	/* EEPROM器件无应答 */
+			goto cmd_fail;	/* EEPROM������Ӧ�� */
 		}
 
-		usAddr++;	/* 地址增1 */		
+		usAddr++;	/* ��ַ��1 */		
 	}
 	
-	/* 命令执行成功，发送I2C总线停止信号 */
+	/* ����ִ�гɹ�������I2C����ֹͣ�ź� */
 	i2c_Stop();
 	return 1;
 
-cmd_fail: /* 命令执行失败后，切记发送停止信号，避免影响I2C总线上其他设备 */
-	/* 发送I2C总线停止信号 */
+cmd_fail: /* ����ִ��ʧ�ܺ��мǷ���ֹͣ�źţ�����Ӱ��I2C�����������豸 */
+	/* ����I2C����ֹͣ�ź� */
 	i2c_Stop();
 	return 0;
 }
@@ -205,35 +205,35 @@ void ee_Erase(void)
 	uint16_t i;
 	uint8_t buf[EEPROM_SIZE];
 	
-	/* 填充缓冲区 */
+	/* ��仺���� */
 	for (i = 0; i < EEPROM_SIZE; i++)
 	{
 		buf[i] = 0xFF;
 	}
 	
-	/* 写EEPROM, 起始地址 = 0，数据长度为 256 */
+	/* дEEPROM, ��ʼ��ַ = 0�����ݳ���Ϊ 256 */
 	if (ee_WriteBytes(buf, 0, EEPROM_SIZE) == 0)
 	{
-		printf("擦除eeprom出错！\r\n");
+		printf("����eeprom������\r\n");
 		return;
 	}
 	else
 	{
-		printf("擦除eeprom成功！\r\n");
+		printf("����eeprom�ɹ���\r\n");
 	}
 }
 
 
 /*--------------------------------------------------------------------------------------------------*/
-static void ee_Delay(__IO uint32_t nCount)	 //简单的延时函数
+static void ee_Delay(__IO uint32_t nCount)	 //�򵥵���ʱ����
 {
 	for(; nCount != 0; nCount--);
 }
 
 
 /*
- * eeprom AT24C02 读写测试
- * 正常返回1，异常返回0
+ * eeprom AT24C02 ��д����
+ * ��������1���쳣����0
  */
 uint8_t ee_Test(void) 
 {
@@ -244,13 +244,13 @@ uint8_t ee_Test(void)
 /*-----------------------------------------------------------------------------------*/  
   if (ee_CheckOk() == 0)
 	{
-		/* 没有检测到EEPROM */
-		printf("没有检测到串行EEPROM!\r\n");
+		/* û�м�⵽EEPROM */
+		printf("û�м�⵽����EEPROM!\r\n");
 				
 		return 0;
 	}
 /*------------------------------------------------------------------------------------*/  
-  /* 填充测试缓冲区 */
+  /* �����Ի����� */
 	for (i = 0; i < EEPROM_SIZE; i++)
 	{		
 		write_buf[i] = i;
@@ -258,25 +258,25 @@ uint8_t ee_Test(void)
 /*------------------------------------------------------------------------------------*/  
   if (ee_WriteBytes(write_buf, 0, EEPROM_SIZE) == 0)
 	{
-		printf("写eeprom出错！\r\n");
+		printf("дeeprom������\r\n");
 		return 0;
 	}
 	else
 	{		
-		printf("写eeprom成功！\r\n");
+		printf("дeeprom�ɹ���\r\n");
 	}
   
-  /*写完之后需要适当的延时再去读，不然会出错*/
+  /*д��֮����Ҫ�ʵ�����ʱ��ȥ������Ȼ�����*/
   ee_Delay(0x0FFFFF);
 /*-----------------------------------------------------------------------------------*/
   if (ee_ReadBytes(read_buf, 0, EEPROM_SIZE) == 0)
 	{
-		printf("读eeprom出错！\r\n");
+		printf("��eeprom������\r\n");
 		return 0;
 	}
 	else
 	{		
-		printf("读eeprom成功，数据如下：\r\n");
+		printf("��eeprom�ɹ����������£�\r\n");
 	}
 /*-----------------------------------------------------------------------------------*/  
   for (i = 0; i < EEPROM_SIZE; i++)
@@ -284,7 +284,7 @@ uint8_t ee_Test(void)
 		if(read_buf[i] != write_buf[i])
 		{
 			printf("0x%02X ", read_buf[i]);
-			printf("错误:EEPROM读出与写入的数据不一致");
+			printf("����EEPROM������д������ݲ�һ��\n");
 			return 0;
 		}
     printf(" %02X", read_buf[i]);
@@ -294,7 +294,7 @@ uint8_t ee_Test(void)
 			printf("\r\n");	
 		}		
 	}
-  printf("eeprom读写测试成功\r\n");
+  printf("eeprom��д���Գɹ�\r\n");
   return 1;
 }
 /*********************************************END OF FILE**********************/
